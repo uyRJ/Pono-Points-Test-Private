9f55313b657fcae3cc622b08374a9f9a419a0999
uyRJ <77024379+uyRJ@users.noreply.github.com>
2021-11-14 19:27:31 -1000

    Update game.js

--- Game Code/game.js
+++ Game Code/game.js
@@ -14,7 +14,7 @@ let availableQuesions = [];
 let questions = [];
 
 fetch(
-    'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
+    'https://opentdb.com/api.php?amount=20'
 )
     .then((res) => {
         return res.json();
